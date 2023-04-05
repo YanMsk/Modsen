588b2f4c8f70e0b7cb6b3d9ad67de15117c34a42
YanMsk <masko.yan@mail.ru>
2023-04-05 21:40:05 +0300

    added questions.js

---
+++ js/questions.js
@@ -0,0 +1,82 @@
+let questions =[
+	{
+		number:1,
+		question:"How many planets are in the solar system?",
+		answer:"8",
+		options:[
+			"8",
+			"9",
+			"10"
+		]
+	},
+
+	{
+		number:2,
+		question:"What is the freezing point of water?",
+		answer:"0",
+		options:[
+			"0",
+			"-5",
+			"-6"
+		]
+	},
+
+	{
+		number:3,
+		question:"What is the longest river in the world?",
+		answer:"Nile",
+		options:[
+			"Amazon",
+			"Nile",
+			"Yangtze"
+		]
+	},
+
+	{
+		number:4,
+		question:"How many chromosomes are in the human genome?",
+		answer:"46",
+		options:[
+			"42",
+			"44",
+			"46"
+		]
+	},
+
+	{
+		number:5,
+		question:"Which of these characters are friends with Harry Potter?",
+		answer:[
+			"Ron Weasley",
+			"Hermione Granger"
+	],
+		options:[
+			"Ron Weasley",
+			"Draco Malfoy",
+			"Hermione Granger"
+		]
+	},
+
+	{
+		number:6,
+		question:"What is the capital of Canada?",
+		answer:"Ottawa",
+		options:[
+			"Ottawa",
+			"Toronto",
+			"Vancouver"
+		]
+	},
+
+	{
+		number:7,
+		question:"What is the Jewish New Year called?",
+		answer:"Rosh Hashanah",
+		options:[
+			"Yom Kippur",
+			"Kwanzaa",
+			"Rosh Hashanah"
+		]
+	}
+]
+
