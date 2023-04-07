f5f8d2c161caa878e7d6a9a7854db18e9b1af850
YanMsk <masko.yan@mail.ru>
2023-04-07 19:17:15 +0300

    added question counter

--- js/script.js
+++ js/script.js
@@ -7,13 +7,22 @@ const quitBtn = document.querySelector(".quit");
 startBtn.onclick = ()=>{
 quizContainer.classList.add("activeQuiz");
 showQuestions(0);
+showQuestionsCounter(1);
 }
 
-let questionsCount = 0;
+let questionCount = 0;
+let questionNumber = 1;
 let nextBtn = document.querySelector(".next-btn")
 nextBtn.onclick = () =>{
-	questionsCount++;
-	showQuestions(questionsCount)
+	
+	if(questionCount < questions.length-1){
+	questionCount++;
+	questionNumber++;
+	showQuestions(questionCount)
+	showQuestionsCounter(questionNumber);
+	}else {
+		console.log('asd');
+	}
 }
 
 function showQuestions(index){
@@ -26,4 +35,11 @@ function showQuestions(index){
 								
 	questionText.innerHTML = questionTag;
 	optionList.innerHTML = optionTag;
+}
+
+
+function showQuestionsCounter(index){
+const totalQuestions = document.querySelector(".header__total-questions");
+let totalQuestionsTag = '<span><p>'+ index +'</p>of<p>'+ questions.length +'</p>Questions</span>';
+totalQuestions.innerHTML = totalQuestionsTag;
 }
